d84ce41ddc886366e67f4f2bb7dde0203362f207
ingkarat <key.ingkarat.29@gmail.com>
2024-11-08 13:10:07 +0700

    add highlight and video Carousel

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -24,6 +24,7 @@ const Hero = () => {
 
     useGSAP(() => {
         gsap.to('#hero', { opacity : 1, delay: 0.5},[])
+        gsap.to('#cta', { opacity : 1,y: -50, delay: 1.5},[])
     })
  
   return (
@@ -36,6 +37,12 @@ const Hero = () => {
                   </video>
               </div>
           </div>
+
+          <div id="cta" className='flex flex-col items-center opacity-0 translate-y-20'>
+              <a href="#hilight" className='btn'> BUY</a>
+              <p>From $199/ month or $999</p>
+          </div>
+          
         </section>
   )
 }
--- src/component/Hilight.jsx
+++ src/component/Hilight.jsx
@@ -1,8 +1,39 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
+import { rightImg, watchImg } from '../utils'
+import VideoC from './VideoC'
+
 
 const Hilight = () => {
+
+    useGSAP(() => {
+        gsap.to('#title', { opacity: 1, y: 0 })
+        gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+    },[])
+
+
   return (
-    <div>Hilight</div>
+    <section id='hilight' className='w-screen overflow-hidden h-full common-padding bg-zinc '>
+        <div className='screen-max-width'>
+            <div className='mb-12 w-full items-end justify-between md:flex'>
+                  <h1 id='title' className='section-heading'>Get the highlights.</h1>
+
+                  <div className='flex flex-wrap items-end gap-10'>
+                      <p className='link'>
+                         Watch the films
+                         <img src={watchImg} alt="ml-1"className='pt-1 px-2' />
+                      </p>    
+                      <p className='link'>
+                         Watch the events
+                         <img src={rightImg} alt="ml-2" className='pt-1 px-3'/>
+                      </p>
+                  </div>
+              </div> 
+              
+            <VideoC/>
+          </div>
+    </section>
   )
 }
 
